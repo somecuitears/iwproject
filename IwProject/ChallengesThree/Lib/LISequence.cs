c27e6f1f8399d95be6cd4347b9ce101f709dba6b
Sumeet Maharjan <msumeetiw@gmail.com>
2018-06-13 18:08:18 +0545

    Challenge three Fixes

--- IwProject/ChallengesThree/Lib/LISequence.cs
+++ IwProject/ChallengesThree/Lib/LISequence.cs
@@ -7,10 +7,11 @@ namespace ChallengesThree.Lib
     public class LISequence
     {
         Array array;
+        int size;
         public void SetArraySize()
         {
-            int.TryParse(Console.ReadLine(), out int value);
-            array = Array.CreateInstance(typeof(int), value);
+            int.TryParse(Console.ReadLine(), out size);
+            array = Array.CreateInstance(typeof(int), size);
         }
 
         public void SetArrayElements()
@@ -24,18 +25,53 @@ namespace ChallengesThree.Lib
 
         public void DisplayResult()
         {
-            int counter = 0, temp = 0;
-            foreach (int item in array)
+            int counter = 1, temp = 0;
+            Array trackingArray = Array.CreateInstance(typeof(int), size);
+            for (int i = 0; i < array.Length; i++)
             {
-                if (temp < item)
+                int remaining = array.Length - i;
+                if (counter > remaining)
+                    break;
+                counter = 1;
+                temp = (int)array.GetValue(i);
+                for (int j = i + 1; j < array.Length; j++)
                 {
-                    counter++;
-                    temp = item;
+                    int temp2 = (int)array.GetValue(j);
+                    if (temp <= temp2)
+                    {
+                        counter++;
+                        temp = (int)array.GetValue(j);
+                    }
                 }
+                trackingArray.SetValue(counter, i);
             }
             Console.WriteLine();
+            Array.Sort(trackingArray);
+            int last = array.Length - 1;
+            Console.WriteLine(trackingArray.GetValue(last));
+            Console.ReadKey();
+        }
+
+        //From Internet...
+        public void DisplayResultFixed()
+        {
+            int[] lis = new int[size];
+            int i, j, max = 0;
+            int[] arr = (int[])array;
+            for (i = 0; i < size; i++)
+                lis[i] = 1;
+
+            for (i = 1; i < size; i++)
+                for (j = 0; j < i; j++)
+                    if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                        lis[i] = lis[j] + 1;
+
+            for (i = 0; i < size; i++)
+                if (max < lis[i])
+                    max = lis[i];
             Console.WriteLine();
-            Console.WriteLine(counter);
+            Console.WriteLine();
+            Console.WriteLine(max);
             Console.ReadKey();
         }
     }
