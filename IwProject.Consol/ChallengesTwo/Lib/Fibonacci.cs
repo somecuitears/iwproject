221485e091507de6763baae8a120fd43ab2d8343
Sumeet Maharjan <msumeetiw@gmail.com>
2018-06-13 14:47:13 +0545

    Challenge Two Completed

---
+++ IwProject.Consol/ChallengesTwo/Lib/Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengesTwo.Lib
+{
+    public class Fibonacci
+    {
+        List<Decimal> fibList = new List<Decimal>();
+
+
+        public List<Decimal> Generator(Decimal upTo)
+        {
+            int a = 0, b = 1;
+            //Console.WriteLine(a);
+            fibList.Add(a);
+            for (int i = 0; i < upTo; i++)
+            {
+                //Console.WriteLine(b);
+                fibList.Add(b);
+                int temp = a;
+                a = b;
+                b = b + temp;
+
+            }
+            return fibList;
+        }
+
+        public Decimal Search(Decimal number)
+        {
+            Decimal a = fibList.IndexOf(number);
+
+            //Console.WriteLine(a);
+            return a;
+        }
+    }
+}
---
+++ IwProject.Consol/ChallengesTwo/Program.cs
@@ -0,0 +1,32 @@
+﻿using ChallengesTwo.Lib;
+using System;
+using System.Collections.Generic;
+
+namespace ChallengesTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fb = new Fibonacci();
+            List<Decimal> myList = fb.Generator(100);
+            Decimal index = fb.Search(Decimal.Parse(Console.ReadLine()));
+            if (index == -1)
+            {
+                Console.WriteLine("IsNotFibo");
+            }
+            else
+            {
+                String vall = "IsFibo ";
+                for (int i = (int)index - 1; i >= index - 2; i--)
+                {
+                    vall += myList[i] + ",";
+                }
+                Console.WriteLine(vall.Substring(0, vall.Length - 1));
+            }
+            //
+            Console.ReadKey();
+
+        }
+    }
+}
