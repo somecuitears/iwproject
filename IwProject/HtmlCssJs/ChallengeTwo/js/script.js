c3ca7b95b34605d72ab856c877f37032a030f537
Sumeet Maharjan <msumeetiw@gmail.com>
2018-06-15 17:19:42 +0545

    Fixed issue with reseting value. Added dispaying data when validation success

--- IwProject/HtmlCssJs/ChallengeTwo/js/script.js
+++ IwProject/HtmlCssJs/ChallengeTwo/js/script.js
@@ -1,132 +1,158 @@
- function Validate() {
-     var index = 0;
-     var preferences = [];
-     var emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i;
-     var fname = document.forms["jsFormValid"]["fname"];
-     var address = document.forms["jsFormValid"]["address"];
-     var zip = document.forms["jsFormValid"]["zip"];
-     var country = document.forms["jsFormValid"]["country"];
-     var gender = document.forms["jsFormValid"]["gender"];
+var isValid = 1;
 
-     //Internet Method...
-     var noSelected = document.querySelectorAll('input[type="checkbox"]:checked').length;
-     if (noSelected == 0) {
-         DisplayError("prefError", "Select One or more color.");
-     } else {
-         DisplayError("prefError", "");
-     }
-     //My Method.
-     /*
-     var elements = document.getElementsByName('color'); for (var i = 0; elements[i]; ++i) { if (elements[i].checked) { preferences[index++] = elements[i].value; } } if (preferences.length == 0) { DisplayError("prefError", "Select One or more color."); } else { DisplayError("prefError", ""); }
-     */
+function Validate() {
+    var index = 0;
+    var emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i;
 
-     var phone = document.forms["jsFormValid"]["phone"];
-     var email = document.forms["jsFormValid"]["email"];
-     var password = document.forms["jsFormValid"]["password"];
-     var confirmPassword = document.forms["jsFormValid"]["confirmPassword"];
+    var fname = document.forms["jsFormValid"]["fname"];
+    var address = document.forms["jsFormValid"]["address"];
+    var zip = document.forms["jsFormValid"]["zip"];
+    var country = document.forms["jsFormValid"]["country"];
+    var gender = document.forms["jsFormValid"]["gender"];
 
-     if (fname.value === "") {
-         DisplayError("nameError", "Please enter your name");
-     } else {
-         DisplayError("nameError", "")
-     }
+    //Internet Method...
+    var noSelected = document.querySelectorAll('input[type="checkbox"]:checked');
 
-     if (isNaN(zip.value) || zip.value.length != 6) {
-         DisplayError("zipError", "Numeric Value and 6 Digit Required");
-     } else {
-         DisplayError("zipError", "")
-     }
+    var noSelLent = noSelected.length;
 
-     if (country.value === "") {
-         DisplayError("countryError", "Please Select Value...");
-     } else {
-         DisplayError("countryError", "")
-     }
+    var preferences = "";
+    //= [noSelected.item(0).value, noSelected.item(1).value, noSelected.item(2).value].filter(Boolean).join(" ");
 
-     if (gender.value === "") {
-         DisplayError("genderError", "Please Choose Gender")
-     } else {
-         DisplayError("genderError", "")
-     }
+    if (noSelLent == 0) {
+        DisplayError("prefError", "Select One or more color.");
+    } else {
+        for (var i = 0; i < noSelLent; i++) {
+            preferences += noSelected.item(i).value + " ";
+        }
+        ClearError("prefError");
+    }
+    //My Method.
+    /*
+    var elements = document.getElementsByName('color'); for (var i = 0; elements[i]; ++i) { if (elements[i].checked) { preferences[index++] = elements[i].value; } } if (preferences.length == 0) { DisplayError("prefError", "Select One or more color."); } else { DisplayError("prefError", ""); }
+    */
 
-     if (isNaN(phone.value) || phone.value.length != 10) {
-         DisplayError("phoneError", "Numeric Value and 10 Digit Required");
-     } else {
-         DisplayError("phoneError", "");
-     }
+    var phone = document.forms["jsFormValid"]["phone"];
+    var email = document.forms["jsFormValid"]["email"];
+    var password = document.forms["jsFormValid"]["password"];
+    var confirmPassword = document.forms["jsFormValid"]["confirmPassword"];
 
-     var pattern = new RegExp(emailRegex);
-     var emailTest = pattern.test(email.value);
-     if (!emailTest) {
-         DisplayError("emailError", "Enter valid email address");
-     } else {
-         DisplayError("emailError", "");
-     }
+    if (fname.value === "") {
+        DisplayError("nameError", "Please enter your name");
+    } else {
+        ClearError("nameError");
 
-     var p1 = password.value.length;
-     var p2 = confirmPassword.value.length;
-     if (p1 == 0 || p2 == 0) {
+    }
 
-         DisplayError("passError", "Password length must be 6-8 characters");
+    if (isNaN(zip.value) || zip.value.length != 6) {
+        DisplayError("zipError", "Numeric Value and 6 Digit Required");
+    } else {
+        ClearError("zipError");
+    }
 
-     } else {
-         if (password.value === confirmPassword.value) {
-             if (p1 < 5 && p1 > 9 || p2 < 5 && p2 > 9) {
-                 DisplayError("passError", "Password length must be 6-8 characters");
-             } else {
-                 DisplayError("passError", "");
-             }
-         } else {
+    if (country.value === "") {
+        DisplayError("countryError", "Please Select Value...");
+    } else {
+        ClearError("countryError");
+    }
 
-             DisplayError("passError", "Password do not match");
-         }
-     }
- }
+    if (gender.value === "") {
+        DisplayError("genderError", "Please Choose Gender")
+    } else {
+        ClearError("genderError");
+    }
 
- function Reset() {
-     //            var a = document.querySelectorAll("input");
-     //            a.forEach(a => {
-     //            var fieldtype = a.type.toLowerCase();
+    if (isNaN(phone.value) || phone.value.length != 10) {
+        DisplayError("phoneError", "Numeric Value and 10 Digit Required");
+    } else {
+        ClearError("phoneError");
+    }
 
-     //Clear Input fields
-     var selectField = document.getElementsByTagName("select");
-     selectField.country.selectedIndex = 0;
+    var pattern = new RegExp(emailRegex);
+    var emailTest = pattern.test(email.value);
+    if (!emailTest) {
+        DisplayError("emailError", "Enter valid email address");
+    } else {
+        ClearError("emailError");
+    }
 
-     var allElements = document.getElementsByTagName("input");
+    var p1 = password.value.length;
+    var p2 = confirmPassword.value.length;
+    if (p1 == 0 || p2 == 0) {
 
-     for (var i = 0; i < allElements.length; i++) {
-         var fieldtype = allElements[i].type.toLowerCase();
-         //console.log(fieldtype);
-         switch (fieldtype) {
-             case "text":
-             case "password":
-             case "textarea":
-                 //alert("text");
-                 allElements[i].value = "";
-                 break;
-             case "radio":
-             case "checkbox":
-                 // alert("box");
-                 if (allElements[i].checked) {
-                     allElements[i].checked = false;
-                 }
-                 break;
-             case "select":
-                 //alert("select");
-                 allElements[i].selectedIndex = 1;
-                 break;
-         }
-     }
+        DisplayError("passError", "Password length must be 6-8 characters");
 
-     //Clear Error msg
-     var allSpan = document.getElementsByTagName("span");
-     console.log(allSpan);
-     for (var i = 0; i < allSpan.length; i++) {
-         allSpan[i].innerText = "";
-     }
+    } else {
+        if (password.value === confirmPassword.value) {
+            if (p1 < 5 && p1 > 9 || p2 < 5 && p2 > 9) {
+                DisplayError("passError", "Password length must be 6-8 characters");
+            } else {
+                // DisplayError("passError", "");
+                ClearError("passError");
+            }
+        } else {
 
- }
+            DisplayError("passError", "Password do not match");
+        }
+    }
 
- function DisplayError(id, message) {
-     document.getElementById(id).innerHTML = "<span>" + message + "</span>"
- }
+    if (isValid != 0) {
+        alert("Name: " + fname.value + "\nAddress: " + address.value + "\nZip Code: " + zip.value + "\nCountry: " + country.value + "\nGender: " + gender.value + "\nColor: " + preferences + "\nPhone: " + phone.value + "\nEmail :" + email.value);
+    }
+
+}
+
+function Reset() {
+    //            var a = document.querySelectorAll("input");
+    //            a.forEach(a => {
+    //            var fieldtype = a.type.toLowerCase();
+
+    //Clear Input fields
+    var selectField = document.getElementsByTagName("select");
+    selectField.country.selectedIndex = 0;
+
+    var allElements = document.getElementsByTagName("input");
+
+    for (var i = 0; i < allElements.length; i++) {
+        var fieldtype = allElements[i].type.toLowerCase();
+        //console.log(fieldtype);
+        switch (fieldtype) {
+            case "text":
+            case "password":
+            case "textarea":
+                //alert("text");
+                allElements[i].value = "";
+                break;
+            case "radio":
+            case "checkbox":
+                // alert("box");
+                if (allElements[i].checked) {
+                    allElements[i].checked = false;
+                }
+                break;
+            case "select":
+                //alert("select");
+                allElements[i].selectedIndex = 1;
+                break;
+        }
+    }
+
+    //Clear Error msg
+    var allSpan = document.getElementsByTagName("span");
+    console.log(allSpan);
+    for (var i = 0; i < allSpan.length; i++) {
+        allSpan[i].innerText = "";
+    }
+
+}
+
+function ClearError(id) {
+    document.getElementById(id).innerText = "";
+    console.log(isValid);
+    isValid = 1;
+}
+
+function DisplayError(id, message) {
+    document.getElementById(id).innerHTML = "<span>" + message + "</span>";
+    console.log(isValid);
+    isValid = 0;
+}
