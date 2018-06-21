642bf80ad741a1035fff85871cb6aa232d99aa17
Sumeet Maharjan <msumeetiw@gmail.com>
2018-06-21 10:12:27 +0545

    Challenge 2 of Responsive Completed

--- IwProject/Responsive/ChallengesTwo/gulpfile.js
+++ IwProject/Responsive/ChallengesTwo/gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 gulp.task('scripts', function() {
-    return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js'])
+    // return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js'])
+    return gulp.src(['src/jquery.min.js', 'src/bootstrap.min.js'])
       .pipe(concat('all.js'))
       .pipe(gulp.dest('./js/'));
   });
